e5a17bf1ee8653ddea4232090b5634052c8e1074
JohKenPo <joaovitormoraes.cc@gmail.com>
2023-03-24 19:37:52 -0300

    Init List 5

---
+++ Codes by subject/List-5-LISTAS/Exe1.c
@@ -0,0 +1,110 @@
+/**
+ * @file Exe1.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-24
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+/*
+Faça uma função recursiva que retorne o número de nós contidos em uma lista L. Considere que L é
+simplesmente encadeada, sem nó cabeça e não circular. Os elementos de L não estão necessariamente
+em ordem.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Type_linked_list
+{
+    int info;
+    struct Type_linked_list *next;
+}TLL;
+
+void init(TLL *lst);
+int empity(TLL *lst);
+int count_node(TLL *lst);
+void insert_start(TLL *lst);
+void print(TLL *lst);
+void clean(TLL *lst);
+
+int main(int argc, char const *argv[])
+{
+    TLL *lst = (TLL*) malloc(sizeof(TLL));
+    if(!lst)exit(1);
+
+    init(lst);
+    print(lst);
+    printf("Number of nodes: %d\n", count_node(lst));
+    insert_start(lst);
+    insert_start(lst);
+    insert_start(lst);
+    print(lst);
+    printf("Number of nodes: %d\n", count_node(lst));
+    clean(lst);
+    
+    return 0;
+}
+
+void init(TLL *lst)
+{
+    lst->next = NULL;
+}
+int empity(TLL *lst)
+{
+    if(lst->next == NULL)return 1;
+    return 0;
+}
+int count_node(TLL *lst)
+{
+   int count = 0 ;
+   TLL *aux = lst;
+
+   while(aux->next != NULL)
+   {
+        
+        count++;
+        aux = aux->next;
+   }
+   return count; 
+}
+void insert_start(TLL *lst)
+{
+    TLL *new = (TLL*) malloc(sizeof(TLL));
+    if(!new)exit(1);
+
+    printf("Enter info: ");
+    scanf("%d", &new->info);
+
+    // TLL *aux = lst->next;
+    new->next = lst->next;
+    lst->next = new;
+
+}
+void print(TLL *lst)
+{
+    
+    if(empity(lst)){
+        printf("-> NULL\n");
+    }else{
+
+        TLL *aux = lst->next; 
+        while(aux != NULL)
+        {
+            printf("| %d | -> ", aux->info);
+            aux = aux->next;
+
+        }
+        printf("NULL\n");
+    }
+}
+void clean(TLL *lst){
+    TLL *aux;
+    while (lst != NULL) {
+        aux = lst;
+        lst = lst->next;
+        free(aux);
+    }
+    printf("CLEAR");
+}
