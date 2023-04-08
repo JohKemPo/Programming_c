b6944bc8a48edf1aea167311fee3dfa22bf944ca
JohKenPo <joaovitormoraes.cc@gmail.com>
2023-04-08 07:47:12 -0300

    Init R7

---
+++ Codes by subject/Lista_revisao_PE/Exe01.c
@@ -0,0 +1,57 @@
+/**
+ * @file Exe01.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-07
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+/*
+Escreva um programa que implemente a Peneira de Eratosthenes. Você deve ler o valor n e
+mostrar todos os números primos encontrados.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int n;
+    printf("Digite um número inteiro n: ");
+    scanf("%d", &n);
+    
+    // criando um vetor para armazenar se um número é primo ou não
+    int *primo = malloc((n+1) * sizeof(int));
+    
+    // inicializando o vetor com todos os valores iguais a true (assumindo que todos os números são primos)
+    for (int i = 2; i <= n; i++) {
+        primo[i] = 1;
+    }
+    
+    // aplicando a peneira
+    for (int i = 2; i*i <= n; i++) {
+        if (primo[i]) {
+            for (int j = i*i; j <= n; j += i) {
+                primo[j] = 0;
+            }
+        }
+    }
+    
+    // mostrando os números primos encontrados
+    printf("Os números primos entre 2 e %d são:\n", n);
+    for (int i = 2; i <= n; i++) {
+        if (primo[i]) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+    
+    free(primo);
+    return 0;
+}
---
+++ Codes by subject/Lista_revisao_PE/Exe02.c
@@ -0,0 +1,20 @@
+/**
+ * @file Exe02.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-07
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+/*
+Implemente um programa que integre e derive polinômios. Cada polinômio é definido por um
+vetor que contém seus coeficientes. Por exemplo, o polinômio de grau dois 3x^2
+ + 2x + 12 terá um
+vetor de coeficientes v = {12,2,3}. Sua integral será I = {0, 12, 1, 1}, equivalente ao polinômio x^3+ x^2 + 12x, 
+e sua derivada será D = {2, 6}, equivalendo ao polinômio 6x + 2. O programa deve,
+infinitamente: (a) receber o valor do maior grau g do polinômio, seguido de (g + 1) coeficientes; (b)
+calcular qual é a integral e a derivada do polinômio informado; e (c) imprimir os novos polinômios
+na tela. Este programa para somente quando o grau g do polinômio for menor ou igual a zero.
+*/
